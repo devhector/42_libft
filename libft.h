2f399be4e0da1f485f8eff20508affe09d5ed946
Hector <hectorhu17@gmail.com>
2021-07-31 15:57:15 -0300

    add some functions

--- libft.h
+++ libft.h
@@ -19,6 +19,7 @@ char	*ft_strnstr(const char	*s1, const char	*s2, size_t	len);
 
 size_t	ft_strlen(const char	*s);
 
-void	ft_memset(void	*s, int	c, size_t	n);
+void	*ft_memset(void	*s, int	c, size_t	n);
+void	*ft_memmov(void	*s1, const void	*s2, size_t	n);
 
 #endif 

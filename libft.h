fd907578ca6302c93a533c3bb47b29a80d6ff553
Hector <hectorhu17@gmail.com>
2021-07-31 15:56:52 -0300

    add some functions

--- libft.h
+++ libft.h
@@ -12,10 +12,13 @@ int		ft_tolower(int	c);
 int		ft_toupper(int	c);
 int		ft_atof(const char	*str);
 int		ft_strncmp(const char	*s1, const char	*s2, size_t	n);
+int		ft_memcmp(const void	*s1, const void	*s2, size_t	n);
 
 char	*ft_strchr(const char	*s, int	c);
 char	*ft_strnstr(const char	*s1, const char	*s2, size_t	len);
 
 size_t	ft_strlen(const char	*s);
+
+void	ft_memset(void	*s, int	c, size_t	n);
 
 #endif 

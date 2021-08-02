2541f9a6f64fbb5193f6b9f79507130aa1c657d3
Hector <hectorhu17@gmail.com>
2021-08-02 16:32:14 -0300

    add 42 header

--- libft.h
+++ libft.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/02 15:32:03 by hectfern          #+#    #+#             */
+/*   Updated: 2021/08/02 16:01:08 by hectfern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
 
@@ -14,12 +26,17 @@ int		ft_atof(const char	*str);
 int		ft_strncmp(const char	*s1, const char	*s2, size_t	n);
 int		ft_memcmp(const void	*s1, const void	*s2, size_t	n);
 
+char	*ft_strdup(const char	*s);
 char	*ft_strchr(const char	*s, int	c);
 char	*ft_strnstr(const char	*s1, const char	*s2, size_t	len);
 
 size_t	ft_strlen(const char	*s);
 
+void	*ft_bzero(void	*s, size_t	n);
+void	*ft_calloc(size_t	n, size_t	size);
 void	*ft_memset(void	*s, int	c, size_t	n);
 void	*ft_memmov(void	*s1, const void	*s2, size_t	n);
+void	*ft_memcpy(void	*s1, const void	*s2, size_t	n);
+void	*ft_memchr(const void	*s, int	c, size_t	n);
 
 #endif 

12e8ba5a8e92241e2bd576fec007326874be7780
Hector <hectorhu17@gmail.com>
2021-08-04 15:30:23 -0300

    create ft_strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/04 15:00:58 by hectfern          #+#    #+#             */
+/*   Updated: 2021/08/04 15:02:50 by hectfern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const	*s1, char const	*set)
+{
+
+}
+
+/*
+Allocates (with malloc(3)) and returns a copy of
+’s1’ with the characters specified in ’set’ removed
+from the beginning and the end of the string.
+return: The trimmed string. NULL if the allocation fails.
+*/

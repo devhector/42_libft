23d5408e4acfcc6989e7c843c1dcd2461f2282d0
Hector <hectorhu17@gmail.com>
2021-08-04 10:53:25 -0300

    fix func

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 16:34:52 by hectfern          #+#    #+#             */
-/*   Updated: 2021/08/04 10:49:06 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/08/04 10:53:19 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ size_t	ft_strlcat(char	*dst, const char	*src, size_t	size)
 	if(len_dst + 1 > size)
 		return (size + len_src);
 	if(len_dst + 1 < size)
-		while (i + len_src < size && src[i])
+		while (i + len_dst < size && src[i])
 		{
 			dst[len_dst + i] = src[i];
 			i++;

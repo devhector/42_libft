fb4ebe798b08bd3705ecddcf44159f9312c9656e
Hector <hectorhu17@gmail.com>
2021-08-04 10:33:34 -0300

    refatoring the func

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 16:34:52 by hectfern          #+#    #+#             */
-/*   Updated: 2021/08/03 15:50:48 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/08/04 10:33:03 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,18 @@ size_t	ft_strlcat(char	*dst, const char	*src, size_t	size)
 {
 	size_t	i;
 	size_t	len_dst;
+	size_t	len_src;
 
 	i = 0;
 	len_dst = ft_strlen(dst);
-	while (i < size && src[i] != '\0')
+	len_src = ft_strlen(src);
+	if(len_dst + 1 < size)
+		return (size + len_src);
+	while (i + len_src < size && src[i])
 	{
 		dst[len_dst + i] = src[i];
 		i++;
 	}
 	dst[len_dst + i] = '\0';
-	return (len_dst + i);
+	return (len_dst + len_dst);
 }

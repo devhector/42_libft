91caf05642f27b1075b7e9851f2c84f6768eced3
Hector <hectorhu17@gmail.com>
2021-08-04 12:46:14 -0300

    fix while in function

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 12:15:49 by hectfern          #+#    #+#             */
-/*   Updated: 2021/08/04 12:17:28 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/08/04 12:43:13 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,13 @@ char	*ft_substr(char const *s, unsigned int pos, size_t len)
 	char	*dst;
 
 	dst = (char *)malloc((sizeof(char *) * len) + 1);
-	if (!dst)
+	if (!dst || !s)
 		return (NULL);
 	i = 0;
-	while (i < len - 1 && s[pos + i] != '\0')
+	while (i < len && s[pos + i] != '\0')
 	{
 		dst[i] = s[pos + i];
+		i++;
 	}
 	dst[i] = '\0';
 	return (dst);

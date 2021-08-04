3c95073adf19450225aff8907e223764b24488a2
Hector <hectorhu17@gmail.com>
2021-08-04 15:57:46 -0300

    fixed and working

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 16:34:38 by hectfern          #+#    #+#             */
-/*   Updated: 2021/08/04 15:31:46 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/08/04 15:54:25 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ char	*ft_strchr(const char	*s, int	c)
 {
 	while (*s)
 	{
-		if (*s == c)
+		if (*s == (char)c)
 			return ((char *)s);
 		s++;
 	}
+	if ((char)c == '\0')
+		return ((char *)s);
 	return (NULL);
 }

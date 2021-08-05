d775aac122806bbbe403417618c6eed2f433e023
Hector <hectorhu17@gmail.com>
2021-08-05 14:32:29 -0300

    fix errors on test

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 16:35:00 by hectfern          #+#    #+#             */
-/*   Updated: 2021/08/02 16:35:01 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/08/05 14:17:13 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@ size_t	ft_strlcpy(char	*dest, const char	*src, size_t	size)
 	size_t	i;
 	size_t	len;
 
-	len = ft_strlen(src);
 	i = 0;
-	while (i < size && i < len)
+	len = ft_strlen(src);
+	if (!size)
+		return (len);
+	while (i < (size - 1) && i < len && src[i])
 	{
 		dest[i] = src[i];
 		i++;

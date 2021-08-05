4be0a3e11349d4f7b195f472b6809d2008f12cc9
Hector <hectorhu17@gmail.com>
2021-08-05 11:17:43 -0300

    fixed but dont work

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 15:00:58 by hectfern          #+#    #+#             */
-/*   Updated: 2021/08/04 17:07:34 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/08/04 20:07:00 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,23 @@
 
 char	*ft_strtrim(char const	*s1, char const	*set)
 {
+    size_t    start;
+    size_t    end;
+    size_t    i;
 
-	if (!s1 || !set)
-		return (NULL);
+    if (!s1)
+        return (0);
+    i = 0;
+    start = 0;
+    end = ft_strlen(s1) - 1;
+    while (s1[i] && ft_strchr(set, s1[i]))
+        start = (i++) + 1;
+    if (start >= ft_strlen(s1))
+        return ((char *)ft_calloc(sizeof(char), 1));
+    i = ft_strlen(s1) - 1;
+    while (i && s1[i] && ft_strchr(set, s1[i]))
+        end = (i--) - 1;
+    return (ft_substr(s1, start, (end - start + 1)));
 }
 
 /*

96efd1386e7b964fb7aa29e686833708a62e8110
Hector <hectorhu17@gmail.com>
2021-08-09 11:42:54 -0300

    fix func

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 16:14:29 by hectfern          #+#    #+#             */
-/*   Updated: 2021/08/03 15:36:53 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/08/09 11:42:46 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void	*s1, const void	*s2, size_t	n)
 	char		*dst;
 	const char	*src;
 
+	if(!s1 || !s2 || !n)
+		return (NULL);
 	dst = (char *)s1;
 	src = (const char *)s2;
 	while (n--)

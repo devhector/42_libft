a5e7bde164181c552b7045954d0b484b1bfa935a
Hector <hectorhu17@gmail.com>
2021-08-09 13:15:53 -0300

    fix func

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 16:34:21 by hectfern          #+#    #+#             */
-/*   Updated: 2021/08/09 13:14:46 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/08/09 13:15:49 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void	*ft_memmove(void	*s1, const void	*s2, size_t	n)
 	
 	dst = (char *)s1;
 	src = (const char *)s2;
-
 	if (dst < src)
 		ft_memcpy(dst, src, n);
 	else

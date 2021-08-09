f3324d816b82bec4528d98bc0d38b297c36b83ed
Hector <hectorhu17@gmail.com>
2021-08-09 15:19:53 -0300

    fix error when s1 contained s2

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 16:35:14 by hectfern          #+#    #+#             */
-/*   Updated: 2021/08/09 15:09:50 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/08/09 15:19:39 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ int	ft_strncmp(const char	*s1, const char	*s2, size_t	n)
 	size_t	i;
 
 	i = 0;
-	if (n == 0)
-		return (0);
 	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (0);
 }

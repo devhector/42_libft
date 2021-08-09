bd8a035bb23d2aa3f700b5ac784b39879e3f2faa
Hector <hectorhu17@gmail.com>
2021-08-09 13:50:50 -0300

    fix error ('\0')

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 16:35:27 by hectfern          #+#    #+#             */
-/*   Updated: 2021/08/09 13:35:13 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/08/09 13:50:20 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ char	*ft_strrchr(const char	*s, int	c)
 {
 	char	*p;
 
-	p = NULL;
-	if (*s == (char)c)
-		p = (char *)s;
+	p = (char *)s + ft_strlen(s);
+	if (c == '\0')
+		return (p);
 	while (*s)
 	{
 		if (*s == (char)c)

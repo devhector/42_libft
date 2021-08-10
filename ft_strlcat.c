559d2d8f9ee034b3f1418f9c06d51d19c377ad83
Hector <hectorhu17@gmail.com>
2021-08-10 09:00:33 -0300

    fix errors

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 16:34:52 by hectfern          #+#    #+#             */
-/*   Updated: 2021/08/04 11:08:15 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/08/10 08:59:00 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,14 @@ size_t	ft_strlcat(char	*dst, const char	*src, size_t	size)
 	i = 0;
 	len_dst = ft_strlen(dst);
 	len_src = ft_strlen(src);
-	if (len_dst + 1 > size)
+	if (len_dst >= size)
 		return (size + len_src);
-	if (len_dst + 1 < size - 1)
+	while (len_dst + 1 < size && src[i])
 	{
-		while (i + len_dst < size && src[i])
-		{
-			dst[len_dst + i] = src[i];
-			i++;
-		}
+		dst[len_dst] = src[i];
+		i++;
+		len_dst++;
 	}
-	dst[len_dst + i] = '\0';
+	dst[len_dst] = '\0';
 	return (len_dst + len_dst);
 }

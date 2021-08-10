2f7ad11dff6aa3e95b28d6fbe4d2a9d1675f6e80
Hector <hectorhu17@gmail.com>
2021-08-10 10:49:30 -0300

    fix errors

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 16:35:21 by hectfern          #+#    #+#             */
-/*   Updated: 2021/08/10 10:41:09 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/08/10 10:49:19 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ char	*ft_strnstr(const char	*s1, const char	*s2, size_t	len)
 			i++;
 		}
 		if (!*s2)
-			return ((char *)(s1 - s2));
+			return ((char *)(s2 - s1));
 		s1++;
 	}
 	return (NULL);

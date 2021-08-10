e9c7e6958384cd0185c79a495b2e88324ed2acec
Hector <hectorhu17@gmail.com>
2021-08-10 09:03:39 -0300

    fix errors

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 16:34:52 by hectfern          #+#    #+#             */
-/*   Updated: 2021/08/10 08:59:00 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/08/10 09:03:35 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,5 @@ size_t	ft_strlcat(char	*dst, const char	*src, size_t	size)
 		len_dst++;
 	}
 	dst[len_dst] = '\0';
-	return (len_dst + len_dst);
+	return (len_dst + len_src);
 }

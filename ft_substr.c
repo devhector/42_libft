22be0876323fa16e473d35a1b3aff433384afa4b
Hector <hectorhu17@gmail.com>
2021-08-13 17:42:18 -0300

    refurbished func

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 12:15:49 by hectfern          #+#    #+#             */
-/*   Updated: 2021/08/13 17:30:33 by hectfern         ###   ########.fr       */
+/*   Updated: 2021/08/13 17:42:11 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ char	*ft_substr(char const *s, unsigned int pos, size_t len)
 	size_t	i;
 	char	*dst;
 
-	if (!s)
+	if (!s || !len)
 		return (NULL);
 	dst = (char *)malloc(sizeof(char) * (len + 1));
 	if (!dst)
 		return (NULL);
 	i = 0;
-	while (i < len && s[pos + i] && (pos + i) <= ft_strlen(s))
+	while (i < len && s[pos + i] && (pos + i) < ft_strlen(s))
 	{
 		dst[i] = s[pos + i];
 		i++;
